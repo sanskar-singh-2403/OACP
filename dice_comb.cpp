dcb8de799891c7d15cc9c425cebef71df2a4d5c6
Sanskar Singh <sanskar.singh@testrigor.com>
2024-03-14 17:00:43 +0530

    Assignment 1

---
+++ dice_comb.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int M=1e+9+7;
+const int N=1e6+10;
+ll dp[N];
+ 
+ll comb(ll n){
+    if(n<0) return 0;
+    if(n==1) return 1;
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    ll ans=comb(n-1)+comb(n-2)+comb(n-3)+comb(n-4)+comb(n-5)+comb(n-6);
+    dp[n]=ans%M;
+    return dp[n];
+}
+ 
+ 
+ 
+int main(){
+    memset(dp,-1,sizeof(dp));    
+    dp[0]=1;
+    int n;cin>>n;
+    ll ans=comb(n);
+    cout<<ans<<'\n';
+    return 0;
+}
---
+++ grid_paths.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ 
+const int M = 1e9+7;
+ 
+vector<vector<int>> dp(1005,vector<int>(1005,-1)); 
+ 
+bool isValid(int i,int j,int n){
+    return (i <n && i >=0 && j <n && j>=0);
+}
+ 
+int f(int i,int j,vector<vector<char>> &grid){
+ 
+    int n = grid.size();
+    if(i == 0 && j ==0 ){
+        if(grid[0][0] == '*'){
+            return 0;
+        }
+        return 1;
+    }
+ 
+    if(!isValid(i,j,n) || grid[i][j] == '*'){
+        return 0;
+    }
+ 
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+ 
+    int up = 0;
+    int left = 0;
+ 
+    up = f(i-1,j,grid) % M;
+    left = f(i,j-1,grid) % M;
+ 
+    return dp[i][j] = (up + left) % M;
+ 
+}
+ 
+int main(){
+ 
+    int n;cin>>n;
+ 
+    vector<vector<char>> grid(n,vector<char>(n,'#'));
+ 
+    for(int i = 0; i < n; i ++){
+        for(int j = 0; j < n ; j++){
+            cin>>grid[i][j];
+        }
+    }
+ 
+    int ans = f(n-1,n-1,grid) < 0 ? 0 : f(n-1,n-1,grid);
+ 
+    cout<<ans;
+ 
+    return 0;
+}
